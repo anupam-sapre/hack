7440673b6a5dd8aaab0053c5614ed411a95f4dbf
Anupam Sapre <asapre@mobileaware.com>
2016-11-03 17:18:15 -0400

    firsd

--- hackthon/api-ai-facebook-master/src/app.js
+++ hackthon/api-ai-facebook-master/src/app.js
@@ -32,7 +32,14 @@ function processEvent(event) {
 
         let apiaiRequest = apiAiService.textRequest(text,
             {
-                sessionId: sessionIds.get(sender)
+                sessionId: sessionIds.get(sender),
+                contexts: [
+                    {
+                        name: "generic",
+                        parameters: {
+                            facebook_user: userName
+                        }
+                    }
             });
 
         apiaiRequest.on('response', (response) => {
